46dcf8451766c16de8d26c06fa168d2d2f0ffd37
akshay pandey <135638593+Akey777@users.noreply.github.com>
2024-02-20 15:13:16 +0530

    Add files via upload

---
+++ instagram.js
@@ -0,0 +1,18 @@
+let clrchange="follow";
+function change(){
+
+if(clrchange==='follow'){
+  clrchange='following';
+  document.querySelector('.followbtn').innerHTML=clrchange;
+}else{
+  clrchange='follow';
+  document.querySelector('.followbtn').innerHTML=clrchange;
+
+}
+
+
+
+
+
+}
+
